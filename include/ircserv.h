037d9272688ea5220c1dd6990323fb3efc22a542
David <david.nevadoc@estudiante.uam.es>
2017-02-22 12:46:52 +0100

    ircstart

---
+++ include/ircserv.h
@@ -0,0 +1,23 @@
+#ifndef IRCSERV_H
+#define IRCSERV_H
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
+#include <strings.h>
+#include <string.h>
+#include <unistd.h> 
+#include <errno.h> 
+#include <signal.h>
+#include <pthread.h>
+
+#define SUCCESS 0
+#define FAILURE -1
+#define MAX_QUEUE 10
+#define BUFF_SIZE 500
+#define MAXHILOS 100
+
+
+#endif
